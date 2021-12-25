44b61425956966839240ed42e05312421bb25e95
Maksim Shpital <johndoe@example.com>
2021-12-25 21:51:51 +0600

    Fix command mistype and add virtual to methods

--- 2/Command.h
+++ 2/Command.h
@@ -11,78 +11,78 @@
 
 class Command {
 public:
-    virtual void execute(Context&) const {};
-    virtual void execute(Context&, int64_t) const {};
-    virtual void execute(Context&, const std::string&) const {};
+    virtual void execute(Context&) const {
+    virtual void execute(Context&, int64_t) const {}
+    virtual void execute(Context&, const std::string&) const {}
 };
 
 class PushCommand : public Command {
 public:
-    void execute(Context&, int64_t) const;
-    void execute(Context&, const std::string&) const;
+    virtual void execute(Context&, int64_t) const;
+    virtual void execute(Context&, const std::string&) const;
 
     virtual ~PushCommand() {}
 };
 
 class PeekCommand : public Command {
 public:
-    void execute(Context&, const std::string&) const;
+    virtual void execute(Context&, const std::string&) const;
 
     virtual ~PeekCommand() {}
 };
 
 class PopCommand : public Command {
 public:
-    void execute(Context&) const;
+    virtual void execute(Context&) const;
 
     virtual ~PopCommand() {}
 };
 
 class AbsCommand : public Command {
 public:
-    void execute(Context&) const;
+    virtual void execute(Context&) const;
 
     virtual ~AbsCommand() {}
 };
 
 class PlusCommand : public Command {
 public:
-    void execute(Context&) const;
+    virtual void execute(Context&) const;
 
     virtual ~PlusCommand() {}
 };
 
 class MinusCommand : public Command {
 public:
-    void execute(Context&) const;
+    virtual void execute(Context&) const;
 
     virtual ~MinusCommand() {}
 };
 
 class MulCommand : public Command {
 public:
-    void execute(Context&) const;
+    virtual void execute(Context&) const;
 
     virtual ~MulCommand() {}
 };
 
 class DivCommand : public Command {
 public:
-    void execute(Context&) const;
+    virtual void execute(Context&) const;
 
     virtual ~DivCommand() {}
 };
 
 class ReadCommand : public Command {
 public:
-    void execute(Context&) const;
+    virtual void execute(Context&) const;
 
     virtual ~ReadCommand() {}
 };
 
 class PrintCommand : public Command {
 public:
-    void execute(Context&) const;
+    virtual void execute(Context&) const;
 
     virtual ~PrintCommand() {}
 };
